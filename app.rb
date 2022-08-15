9f7322094fb8791bd8c4eb58523398ce5c12276f
allanstar-byte <allanmutisya263@gmail.com>
2022-08-15 12:25:57 +0300

    tests

---
+++ app.rb
@@ -0,0 +1,23 @@
+# app.rb
+# puts "Hello world!"
+
+# p "Hello world 1!"
+
+# pp "Hello world 2!"
+
+# puts [1, 2, 3]
+
+# p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+
+
+# Tests
+
+puts "Hello world!"
+
+print ("Pass this test, please.")
+
+p [1, 2, 3]
